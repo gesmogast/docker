b1f2db6f1adf86fa8a6118b69d1dc945f7f7124c
Anatoli Zbirouski <armagast@gmail.com>
2017-03-27 17:48:44 +0000

    Added magento 1.9.3.2 shell startup script

---
+++ docker-magento1-dev/run-test-magento-environment.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Run docker-compose up
+docker-compose up -d
+# Do post-installation tasks
+echo "Wait 5 seconds to compleately start containers"
+sleep 5s
+containername=$(docker ps --filter "name=magento_1" | awk '{if(NR>1) print $NF}')
+echo "Configuring site name"
+# Site name = script's first argument, otherwise it is testmagento.org
+sitename=${1:-testmagento.org}
+# Check if sitename is configured
+if [[ "$sitename" != "testmagento.org" ]]
+then
+    echo "Replacing default nginx testmagento.org server configuration with $sitename"
+    # Using sed to replace nginx default configured server name
+    docker exec $containername sed -i 's/testmagento.org/$sitename/g' /etc/nginx/sites-available/magento.conf
+    # Reloading nginx
+    docker exec $containername /etc/init.d/nginx reload
+fi
