52997f580081d44ea7e1726bdee2fd6f2c482ded
Anatoli Zbirouski <armagast@gmail.com>
2017-04-09 15:27:04 +0000

    Added magento1 persistence

--- docker-magento1-dev/run-test-magento-environment.sh
+++ docker-magento1-dev/run-test-magento-environment.sh
@@ -8,12 +8,14 @@ containername=$(docker ps --filter "name=magento_1" | awk '{if(NR>1) print $NF}'
 echo "Configuring site name"
 # Site name = script's first argument, otherwise it is testmagento.org
 sitename=${1:-testmagento.org}
+# Make soft-link
+docker exec $containername ln -s /etc/nginx/sites-available/magento.conf /etc/nginx/sites-enabled/magento.conf
 # Check if sitename is configured
 if [[ "$sitename" != "testmagento.org" ]]
 then
     echo "Replacing default nginx testmagento.org server configuration with $sitename"
     # Using sed to replace nginx default configured server name
     docker exec $containername sed -i 's/testmagento.org/$sitename/g' /etc/nginx/sites-available/magento.conf
-    # Reloading nginx
-    docker exec $containername /etc/init.d/nginx reload
 fi
+# Reloading nginx
+docker exec $containername /etc/init.d/nginx reload
